943a86472d86035574c683265261bc7adbd6a28e
Florian RADUREAU <florian.radureau@conserto.pro>
2022-04-03 08:44:09 +0200

    Add initial set of files

---
+++ vendor/composer/installed.php
@@ -0,0 +1,50 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'v5.4.99',
+        'version' => '5.4.99.0',
+        'type' => 'project',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => NULL,
+        'name' => 'symfony/skeleton',
+        'dev' => true,
+    ),
+    'versions' => array(
+        'symfony/flex' => array(
+            'pretty_version' => 'v2.1.6',
+            'version' => '2.1.6.0',
+            'type' => 'composer-plugin',
+            'install_path' => __DIR__ . '/../symfony/flex',
+            'aliases' => array(),
+            'reference' => 'dd7dafe3bfebf8441e167b76d8a18974585467f3',
+            'dev_requirement' => false,
+        ),
+        'symfony/polyfill-ctype' => array(
+            'dev_requirement' => false,
+            'replaced' => array(
+                0 => '*',
+            ),
+        ),
+        'symfony/polyfill-iconv' => array(
+            'dev_requirement' => false,
+            'replaced' => array(
+                0 => '*',
+            ),
+        ),
+        'symfony/polyfill-php72' => array(
+            'dev_requirement' => false,
+            'replaced' => array(
+                0 => '*',
+            ),
+        ),
+        'symfony/skeleton' => array(
+            'pretty_version' => 'v5.4.99',
+            'version' => '5.4.99.0',
+            'type' => 'project',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => NULL,
+            'dev_requirement' => false,
+        ),
+    ),
+);
